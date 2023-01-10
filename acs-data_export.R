19578bbbf71dc5c506c31a24b55af53552ef442d
konorevole10 <kathleen.onorevole@erg.com>
2023-01-10 15:36:26 -0500

    Final code. Cleaning up code and adding more detail to comments.

--- acs-data_export.R
+++ acs-data_export.R
@@ -21,53 +21,29 @@ load("P:/Steam Supplemental/11 EJ Coordination/from EPA/ACS/acs_data_2019_block 
 #Remove geometry from ACS data. This changes it from simple feature to data frame format. Confirmed that leading zeros in GEOID were retained.
 data2 <- st_set_geometry(data, NULL)
 
-#Change GEOID variable from character to numeric for semi_join to work.
-#data3 <- data2 %>%
- # mutate(GEOID = as.numeric(GEOID))
-
 #Outside of R, set up DSN to connect to new blank Access database through ODBC. Created a User DSN named "ACS-data-raw".
 
 #Connect to the blank Access database using the Access 2007 version of the ODBC connection command.
 db_conn <- odbcConnectAccess2007("P:/Steam Supplemental/11 EJ Coordination/01 - Demographics analysis/acs-data-exported_01062023.accdb")
 
-#Create dataframe in R with Census blocks without loads for 2022. These were identified by using Kristi's 'Crosswalk_092122.accdb' database in the same location, and filtering table 'Final_COMID to FIPs Crosswalk' on "Baseline_2022" = 1. I exported the resulting 188 FIPS codes to Excel and saved as a CSV.
-#**Note that this is different from the list of FIPS codes used for the JTA analysis coding, because those included COMIDs without loads. Also note that there are duplicates in this list.
+#Load list of Census blocks with loads for the 2022 Proposal EA. These were identified by using Kristi's 'Crosswalk_092122.accdb' database in the following filepath: "P:\Steam Supplemental\10 EA\04 Supplemental Analyses\Census Block GIS". In Access, I filtered table 'Final_COMID to FIPs Crosswalk' on "Baseline_2022" = 1. I exported the resulting 188 FIPS codes to Excel and saved as a .txt file.
+#**Note that this is different from the list of FIPS codes used for the JTA analysis coding, because those included COMIDs without loads. Also note that there are duplicate FIPS codes in this list. There are 188 entries in the FIPS to COMID crosswalk, and 177 unique FIPS codes.
 
-#FIPStext <- read.delim("P:/Steam Supplemental/11 EJ Coordination/01 - Demographics analysis/FIPS_list.txt", as.is = TRUE, stringsAsFactors = FALSE)
-
-#Reading in a .txt version of the FIPS list (from the COMID crosswalk in the same file location). Used .txt file and set the columns to be characters to avoid losing the leading zeros.
+#Reading in the .txt version of the FIPS list. Set the columns to be characters to avoid losing the leading zeros.
 FIPSlist <- read.table(file = "P:/Steam Supplemental/11 EJ Coordination/01 - Demographics analysis/FIPS_list.txt", header = TRUE, sep = "\t",
                     comment.char = "", colClasses = 'character')
 
-
-#Create dataframe in R with unique Census blocks for 2022. Note that this is different from the original 'CBlist' above, which was based on 2020.
-#uniqueFIPS <- read.csv("P:/Steam Supplemental/10 EA/04 Supplemental Analyses/Census Block GIS/unique-census-blocks-with-loads.csv", header = FALSE)
-
-#uniqueFIPS2 <- read.csv("P:/Steam Supplemental/10 EA/04 Supplemental Analyses/Census Block GIS/Jan6FIPS.csv", colClasses=c("numeric"))
-
-#names(uniqueFIPS) <- c('FIPS')
-
-#nochar <- uniqueFIPS2 %>%
-  #mutate_if(is.numeric, as.integer)
-
-#select(numtest$GEOID = )
-
-#Rename column heading to avoid formatting issues
-#names(uniqueFIPS) <- c('FIPS')
-#numFIPS <- uniqueFIPS %>%
-  #mutate(FIPS = as.numeric(FIPS))
-
-#Create subset with only 188 unique Census blocks of interest.Filter the ACS data to only include rows data for the 222 unique Census blocks. 
+#Create subset of data from Julia with only 177 unique Census blocks of interest. Filter the ACS data to only include rows matching the FIPS codes from the crosswalk .txt file.
 ACS_filtered <- semi_join(x= data2, y= FIPSlist, by= c("GEOID" = "FIPS"))
 
-#Confirming that the number of unique GEOID codes in the filtered table matched the number of unique FIPS codes in the crosswalk (177 in both cases).
+#Confirm that the number of unique GEOID codes in the filtered table matches the number of unique FIPS codes in the crosswalk (177 in both cases).
 length(unique(ACS_filtered$GEOID))
 length(unique(FIPSlist$FIPS))
 
-#Export the extracted data to the database.
+#Export the extracted ACS data to the database.
 sqlSave(db_conn, ACS_filtered, rownames = FALSE, colnames = FALSE, safer = FALSE, addPK = FALSE, fast = FALSE)
 
-#Create list of unique FIPS codes with loads for 2022 Proposal EA.
+#Create list of unique FIPS codes with loads for 2022 Proposal EA. Convert to a data frame for export to Access.
 Unique_FIPS2022 <- unique(ACS_filtered$GEOID)
 Unique_FIPS2022_df <- data.frame(Unique_FIPS2022)
 
